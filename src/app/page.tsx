a6e64ee7621a45e6eeb1b47d0acc2a07edb1152c
edwinmvk <edwinmoncy@gmail.com>
2025-08-03 19:53:01 +0530

    feat: Setup tRPC for data access layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default async function RootPage() {
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+export default function RootPage() {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.createAPI.queryOptions({ text: "hello" }));
+
   return (
     <div>
-      <Button>RootPage</Button>
+      <Button>{JSON.stringify(data)}</Button>
     </div>
   );
 }
